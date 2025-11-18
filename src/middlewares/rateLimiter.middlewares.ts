60f43361aa9a659ba873e3e57f9cb016d4397e88
lovanto <rifkylovanto@gmail.com>
2025-11-18 11:58:10 +0700

    again and again

--- src/middlewares/rateLimiter.middlewares.ts
+++ src/middlewares/rateLimiter.middlewares.ts
@@ -2,6 +2,8 @@ import rateLimit from 'express-rate-limit';
 
 // Rate limiting middleware
 export const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+  windowMs: 2 * 1000, // 2 seconds
+  max: 50, // limit each IP to 50 requests per window
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
