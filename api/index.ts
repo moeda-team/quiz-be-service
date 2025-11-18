c3d078546d415bb82160c19527df2f3b5634cf1f
lovanto <rifkylovanto@gmail.com>
2025-11-18 11:55:47 +0700

    lemme cook

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,17 @@
 import serverless from 'serverless-http';
 import { app } from '../src/app';
+import { config } from '../src/config';
+import { logger } from '../src/utils/common/logger';
+
+try {
+  app.listen(config.port, () => {
+    logger.info('Starting');
+    logger.info(`Server is running on port ${config.port}`);
+    logger.info(`API is available at ${config.apiPrefix}`);
+  });
+} catch (error) {
+  logger.error('Error starting server:', error);
+  process.exit(1);
+}
 
 export default serverless(app);
