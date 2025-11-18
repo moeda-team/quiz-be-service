512c261f84c9ce14451b3c18c66185dafeab694b
lovanto <rifkylovanto@gmail.com>
2025-11-18 16:08:45 +0700

    last

--- src/app.ts
+++ src/app.ts
@@ -32,8 +32,8 @@ app.use(
 );
 
 app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const router = Router();
 router.use(`/users`, userRouter);
